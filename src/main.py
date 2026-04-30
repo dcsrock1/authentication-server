4280bec09b9d912199d5e9279399a14598d065d3
Charles Fletcher <fletcherdcharles@gmail.com>
2026-04-30 13:42:37 +0100

    completed logging for all current request classes

--- src/main.py
+++ src/main.py
@@ -2,114 +2,156 @@ from flask import Flask, request, jsonify, abort
 from flask_restful import Api, Resource
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-import logging
+import datetime
+import json
 import os
 
 import db_manage
+
+API_KEY = os.environ["INTERNAL_API_KEY"]
+ALLOWED_IPS = ["192.168.1.1"]
+LOG_PATH = "auth_events.json"
 
 app = Flask(__name__)
 api = Api(app)
 limiter = Limiter(get_remote_address, app=app, default_limits=["200 per day", "50 per hour"])
 
-logging.basicConfig(
-    filename="auth.log",
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s"
-)
+def log_event(event_type: str, severity: str, details: dict={}) -> None:
+    event = {
+        "timestamp": datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        "severity": severity,
+        "event": event_type,
+        "ip": request.remote_addr,
+        "request_method": request.method,
+        "request_path": request.path,
+        "request_content_type": request.content_type
+        **details
+    }
+    if os.path.exists(LOG_PATH):
+        with open(LOG_PATH, "r") as data:
+            log = json.load(data)
+    else:
+        log = []
+    log.append(event)
 
-API_KEY = os.environ["INTERNAL_API_KEY"]
+    with open(LOG_PATH, "w") as data:
+        json.dump(log)
 
-ALLOWED_IPS = ["192.168.1.1"]
 
 @app.before_request
 def check_ip_and_key():
     ip = request.remote_addr
     key = request.headers.get("X-Internal-Key")
     if ip not in ALLOWED_IPS:
-        logging.warning(f"Unauthorised IP address attempted to send a request from {ip}")
+        log_event("ip_rejected", "warning")
         abort(403)
     if key != API_KEY:
-        logging.warning(f"API key has not been sent or is not correct, they key sent was {key} request originated from {ip}")
+        log_event("API_key_rejected", "warning")
         abort(403)
 
-class Register(Resource):
-    def post(self):
-        data = request.get_json()
-        try:
-            db_manage.register(data["username"], data["password"])
-            return {"message": "User created"}, 201
-        except ValueError as e:
-            logging.warning(f"A user creation request failed due to the username already being in use username: {data["username"]}")
-            return {"error": str(e)}, 409
-        except Exception as e:
-            logging.error(f"An error has occurred: {str(e)}")        
-            return {"error": str(e)}, 500
 
-class Login(Resource):
+class Login(Resource): # done
     def post(self):
         data = request.get_json()
         try:
             id = db_manage.verify_password(data["username"], data["password]"])
             if not id:
+                log_event("login_failed", "warning", {"username": data["username"], "reason": "username or password incorrect"})
                 return {"error": "Username or password is incorrect"}, 401
             else:
+                log_event("login_successful", "info", )
                 return {"message": "Successful login", "token": db_manage.create_token(db_manage.get_user_id(data["username"]))}, 200
         except Exception as e:
-            logging.error(f"An error has occurred: {str(e)}")
+            log_event("login_failed", "error", {""})
+            return {"error": str(e)}, 500
+
+class Register(Resource): # done
+    def post(self):
+        data = request.get_json()
+        token = request.headers.get("Authorization", "").removeprefix("Bearer ")
+        if not token:
+            log_event("no_token", "warning")
+            return {"error": "No token provided"}, 401
+        try:
+            db_manage.register(data["username"], data["password"], data["role"])
+            log_event("user_created", "info")
+            return {"message": "User created"}, 201
+        except ValueError as e:
+            log_event("user_creation_failed", "error", {"reason": f"username {data["username"]} is already in use"})
+            return {"error": str(e)}, 409
+        except Exception as e:
+            log_event("user_creation_failed", "error", {"new_username": data["username"], "error_data": str(e)})     
             return {"error": str(e)}, 500
         
-class RevokeToken(Resource):
+class RevokeToken(Resource): # done
     def post(self):
         token = request.headers.get("Authorization", "").removeprefix("Bearer ")
         data = request.get_json()
         if not token:
+            log_event("no_token", "warning")
             return {"error": "No token provided"}, 401
         user_id = db_manage.verify_token(token)
         if user_id == False:
+            log_event("invalid_token", "warning")
             return {"error": "Invalid or expired token"}, 401
         else:
             db_manage.revoke_token(data["target"])
+            log_event("token_revoked", "info", {"user_id": user_id, "token_id": token[-10:]})
             return {"message": "Token has been successfully revoked"}, 204
         
-class RevokeAllTokens(Resource):
+class RevokeAllTokens(Resource): # done
     def post(self):
         token = request.headers.get("Authorization", "").removeprefix("Bearer ")
         if not token:
+            log_event("no_token", "warning")
             return {"error": "No token provided"}, 401
         user_id = db_manage.verify_token(token)
         if user_id == False:
+            log_event("invalid_token", "warning")
             return {"error": "Invalid or expired token"}, 401
         else:
             db_manage.revoke_all_tokens(user_id)
+            log_event("tokens_revoked", "info", {"user_id": user_id})
             return {"message": "All tokens have been successfully revoked"}, 204
         
 
-class ChangeUsername(Resource):
+class ChangeUsername(Resource): # done
     def post(self):
         token = request.headers.get("Authorization", "").removeprefix("Bearer ")
         data = request.get_json()
         if not token:
+            log_event("no_token", "warning")
             return {"error": "No token provided"}, 401
         user_id = db_manage.verify_token(token)
         if user_id == False:
+            log_event("invalid_token", "warning")
             return {"error": "Invalid or expired token"}, 401
         else:
-            try:
-                db_manage.change_username(user_id, data["new_username"])
-                return {"message": "Username has been changed"}, 204
-            except ValueError as e:
-                return {"error": "Username already in use"}, 409
+            if db_manage.get_role(user_id) != "admin":
+                log_event("authorization_invalid", "warning", {"user_id": user_id})
+                return {"error": "Account does not have authorization"}, 403
+            else:
+                try:
+                    db_manage.change_username(user_id, data["new_username"])
+                    log_event("username_changed", "info", {"user_id": user_id})
+                    return {"message": "Username has been changed"}, 204
+                except ValueError as e:
+                    log_event("username_exists", "warning", {"user_id": user_id})
+                    return {"error": "Username already in use"}, 409
             
 class ChangePassword(Resource):
     def post(self):
         token = request.headers.get("Authorization", "").removeprefix("Bearer ")
         data = request.get_json()
         if not token:
+            log_event("no_token", "warning", {"user_id": user_id})
             return {"error": "No token provided"}, 401
         user_id = db_manage.verify_token(token)
         if user_id == False:
+            log_event("invalid_token", "warning", {"user_id": user_id})
             return {"error": "Invalid or expired token"}, 401
         else:
             db_manage.change_password(user_id, data["new_password"])
+            log_event("password_changed", "info", {"user_id": user_id})
             return {"message": "password has been changed"}, 204
         
